e0d75e4e7b77560d025cef3d69ebf69e29315f50
tishajoshi <43406209+tishajoshi@users.noreply.github.com>
2018-09-29 10:44:13 +0530

    Add files via upload

---
+++ dl54rcksci.py
@@ -0,0 +1,37 @@
+import random
+a={1:'r',2:'p',3:'s'}
+
+while True:
+P=input("your choice r/p/s:")
+c=a[random.randint(1,3)]
+
+print ("you chose:",p,"computer chose:",c)
+while (a=1):
+	if (1==r):
+		print("its a draw")
+		if (1==s):
+			print("you lost")
+			if(1==p):
+				print("you win")
+
+while (a=2):
+	if(2==r):
+		print("you lost")
+		if(2==p):
+			print("its a tie")
+			if(2==s):
+				print("you win")
+
+
+while (a==3):
+	if(3==r):
+		print("you win")
+		if(3==p):
+			print("you lost")
+			if(a==s):
+				print("its a draw")
+
+
+
+
+
