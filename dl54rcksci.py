9bb06e9ef9289af6ae75550780351fd501d63e54
tishajoshi <43406209+tishajoshi@users.noreply.github.com>
2018-10-04 15:49:28 +0530

    Add files via upload

--- dl54rcksci.py
+++ dl54rcksci.py
@@ -1,35 +1,24 @@
 import random
-a={1:'r',2:'p',3:'s'}
-
+player=0
+computer=0
+s={1:'r',2:'p',3:'s'}
 while True:
-P=input("your choice r/p/s:")
-c=a[random.randint(1,3)]
-
-print ("you chose:",p,"computer chose:",c)
-while (a=1):
-	if (1==r):
-		print("its a draw")
-		if (1==s):
-			print("you lost")
-			if(1==p):
-				print("you win")
-
-while (a=2):
-	if(2==r):
-		print("you lost")
-		if(2==p):
-			print("its a tie")
-			if(2==s):
-				print("you win")
-
-
-while (a==3):
-	if(3==r):
+	comp=s[random.randint(1,3)]
+	r=input("enter choice r/p/s")
+	if(r=='r' and comp=='s') or (r=='p'and comp=='r') or (r=='s' and comp=='p'):
 		print("you win")
-		if(3==p):
-			print("you lost")
-			if(a==s):
-				print("its a draw")
+		player=player+1
+	else:
+		print('comp wins')
+		computer=computer+1
+	if(player==3):
+		print("Player is the winner")
+		break
+	elif(computer==3):
+		print("Computer is the winner")
+		break
+	
+
 
 
 
